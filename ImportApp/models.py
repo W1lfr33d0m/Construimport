9f906375bd9afd31fed4b1d4492c1588d1b6646d
W1lfr33d0m <wilfreferreira3@gmail.com>
2021-11-01 21:27:28 -0400

    clientes

--- ImportApp/models.py
+++ ImportApp/models.py
@@ -4,6 +4,19 @@ from django.db import models
 from django.db.models.fields import reverse_related
 
 # Create your models here.
+class Cliente(models.Model):
+    NumContratoCliente = models.models.CharField(_("Codigo"), max_length=50)
+    NombreCliente = models.models.TextField(_("Nombre"), max_length=100)
+    
+
+    class Meta:
+        verbose_name = ("Cliente")  
+        verbose_name_plurl = ("Clientes")
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return Reversible("cliente_detail", kwargs={"pk": self.pk})
 class Proveedor(models.Model):
     NumContratoProveedor = models.models.CharField(_("Codigo"), max_length=50)
     NombreProveedor = models.models.TextField(_("Nombre"), max_length=100)
