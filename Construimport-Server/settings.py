3a51bafce281569f1782b1e194d264018a3c30e8
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-01-24 07:47:18 -0500

    24-01-2022

--- Construimport-Server/settings.py
+++ Construimport-Server/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'Solicitudes',
+    'COMEX',
     #'charts_dashboard',
     'reports',
     'djangobower',
