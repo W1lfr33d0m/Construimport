a1abe3662a99ee58282942269d26ace28626c71e
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-02-07 19:50:34 -0500

    07-02-2022-19-50

--- Solicitudes/admin.py
+++ Solicitudes/admin.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 from django import forms
 from django.shortcuts import render
-from .models import Solicitud, Cliente, Proveedor
+from .models import Solicitud
 from django.views.generic.base import TemplateView
-
+from Nomencladores.models import Producto, ProductoForm
 
 # Register your models here.
 @admin.register(Solicitud)
 class SolicitudAdmin(admin.ModelAdmin):
     change_list_template = 'smuggler/change_list.html'
-    list_display = ('numsolicitud', 'numcontratocliente', 'cantidad', 'idproducto', 'fechasol', 'numcontratoproveedor', 'aprobada')
+    list_display = ('numsolicitud', 'fechasol', 'aprobada')
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
-        #form.base_fields['numsolicitud'].label = 'Número'
-        #form.base_fields['numsolicitud'].widget = forms.HiddenInput()
-        form.base_fields['numcontratocliente'].label = 'Cliente'
-        form.base_fields['idproducto'].label = 'Producto'
+        #form.base_fields['numcontratocliente'].label = 'Cliente'
+        #form.base_fields['cantidad'].label = 'Cantidad'
         form.base_fields['fechasol'].label = 'Fecha'
-        form.base_fields['numcontratoproveedor'].label = 'Proveedor'
+        #form.base_fields['idproducto'].label = 'Producto'
+        #form.base_fields['numcontratoproveedor'].label = 'Proveedor'
         
         return form
+    #form = ProductoForm
+    
