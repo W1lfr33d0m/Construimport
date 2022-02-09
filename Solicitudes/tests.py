4153e9fa80291bec0fa1391fdf0ad0f23cd291d1
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-02-08 22:19:19 -0500

    07-02-2022-10-19

--- Solicitudes/tests.py
+++ Solicitudes/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from Solicitudes.models import Solicitud
+from datetime import date
 
+
+class SolicitudTesTcase(TestCase):
+    def setUp(self):
+        day = date.today()
+        Solicitud.objects.create(numsolicitud = '2200001', fechasol = day, aprobada = False)
+        
+    def test_auth_user(self):
+        solicitud1 = Solicitud.objects.get(numsolicitud = '2200001')
+        self.assertEqual(Solicitud.__add__(solicitud1), 'Solicitud insertada')
 # Create your tests here.
