92cc19fb30e6e7bd21cae891130904d39687e768
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-02-09 19:49:16 -0500

    09-02-2022-19-50

--- Trazas/models.py
+++ Trazas/models.py
@@ -1,3 +1,10 @@
+from tabnanny import verbose
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+class LogEntry(models.Model):
+    
+    class meta:
+        verbose_name = ('Traza')
+        verbose_name_plural = ('Trazas')
