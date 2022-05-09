e3c0fe27526a686640726de27d9a43d235b8f760
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-05-09 09:11:43 -0400

    09-05-2022-09-11

--- Trazas/admin.py
+++ Trazas/admin.py
@@ -1,10 +1,11 @@
+from os import remove
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry
 # Register your models here.
 from import_export import resources, widgets, fields
 from import_export.admin import ImportExportModelAdmin
 from import_export.widgets import ForeignKeyWidget
-
+from django.http import HttpRequest
 class LogEntryResource(resources.ModelResource):
     
     class Meta:
@@ -12,6 +13,7 @@ class LogEntryResource(resources.ModelResource):
         skip_unchanged = True
         report_skipped = False
         fields = ('user', 'action_flag', 'content_type', 'action_time', 'object_id', 'object_repr')
+        
 
 @admin.register(LogEntry)
 class LogEntryAdmin(ImportExportModelAdmin):
@@ -39,3 +41,7 @@ class LogEntryAdmin(ImportExportModelAdmin):
         'object_repr'
         
     ]
+    
+    def get_fields(self, request: HttpRequest, obj):
+        return['user', 'action_flag', 'content_type', 'action_time', 'object_id', 'object_repr']
+        return super().get_fields(request, obj)
