72a67c32c101d8122e1b26336f3dde0ada8ee046
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-05-15 19:54:22 -0400

    15-05-2022-19-54

--- Solicitudes/views.py
+++ Solicitudes/views.py
@@ -11,7 +11,49 @@ from .forms import *
 from django.views.generic import CreateView
 #from Solicitudes.models import Solicitud
         
-        
+class Agregar_Solicitud_Equipo(SessionWizardView):
+    model = Solicitud_Equipo
+    form_list = [FSolicitud_Equipo, FSolicitud_Equipo_Proxy]
+    template_name = 'solicitud_equipo_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nombre_url'] = 'solicitud_equipo'
+        context['opts'] = Solicitud_Equipo._meta,
+        context['app_list']
+        context['change'] = True,
+        context['is_popup'] = False,
+        context['save_as'] = False,
+        context['has_delete_permission'] = False,
+        context['has_add_permission'] = True,
+        context['has_change_permission'] = False
+        context['changeform_template'] = 'solicitud_equipo_form.html'
+        context['nombre_formulario'] = 'Agregar Solicitud de Equipo'
+        context['mensaje'] = 'La solicitud fue adicionada correctamente.'
+        return context
+
+    def done(self, form_list, **kwargs):
+        return render(self.request, 'change_list.html', {
+        'form_data': [form.cleaned_data for form in form_list],
+        })
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            return self.render(self.get_form())
+        except KeyError:
+             return super().get(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['numcontratocliente'].widget.attrs.update({'autofocus': 'autofocus'})
+        return form      
+       
+def display(request):
+    return render(request, 'solicitud_equipo_form.html', {'context': Agregar_Solicitud_Equipo.get_context_data})
+
 def generatePDF(request, id):
     buffer = io.BytesIO()
     x = canvas.Canvas(buffer)
@@ -21,25 +63,3 @@ def generatePDF(request, id):
     buffer.seek(0)
     return FileResponse(buffer, as_attachment=True, filename='attempt1.pdf')
 
-class DashboardView(TemplateView):
-    template_name = 'dashboard.html'
-    
-    def get_solicitudes_mensuales(self):
-        eq=0
-        ppa=0
-        nm=bt=0
-        solicitudes = [eq, ppa, nm, bt]
-        year = datetime.now().year
-        for m in range(1, 13):
-            eq = Solicitud_Equipo.objects.filter(fechasol__year = 2022, fechasol__month = m).count()
-            ppa = Solicitud_PPA.objects.filter(fechasol__year = 2022, fechasol__month = m).count()
-            nm = Solicitud_Neumatico.objects.filter(fechasol__year = 2022, fechasol__month = m).count()
-            bt = Solicitud_Bateria.objects.filter(fechasol__year = 2022, fechasol__month = m).count()
-        print(solicitudes)
-        return solicitudes
-        
-    def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        context['panel']='Panel de Administración'
-        context['get_solicitudes_mensuales']=self.get_solicitudes_mensuales()
-        return context
