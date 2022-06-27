280b9f6f1045a06537f3708a404b4dfd0916b3e1
W1lfr33d0m <wilfreferreira3@gmail.com>
2022-06-27 10:20:18 -0400

    27-06-2022

--- Nomencladores/models.py
+++ Nomencladores/models.py
@@ -11,7 +11,7 @@ import os
 from random import choices
 from sqlite3 import Date
 from django.db import models
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from tabnanny import verbose
 from django.db import models
 from operator import contains
@@ -23,7 +23,7 @@ from django.utils import timezone
 from Nomencladores.validators import UnicodenameValidator, UnicodeCodeValidator, UnicodeREEUPValidator, UnicodProveedorValidator, UnicodePersonNameValidator
 from django import forms
 from django.utils.text import slugify
-
+from django.utils import timezone
 
 """
 Clase País
@@ -160,15 +160,22 @@ def validate_representante(representante):
         if i.isnumeric():
             raise ValidationError(_('%(representante)s solo puede contener letras'), params={'representante': representante},)
 
+def fecha_caducidad_cliente():
+    delta = timedelta(days=365)
+    fecha_caducidad = timezone.now() + delta
+    return fecha_caducidad
+
 class Cliente(Empresa):
+    
+    delta = timezone.timedelta(days=365*5) # create a timedelta object
     
     person_name_validator = UnicodePersonNameValidator()
     
     OSDE = models.ForeignKey(OSDE, models.DO_NOTHING, null=False, default='GEDIC')
     codigoprovincia = models.ForeignKey(Provincia, on_delete=models.PROTECT, db_column='codigoprovincia', default='HB', verbose_name='Provincia')
     representante = models.CharField(max_length=40, null=False, validators=[person_name_validator], verbose_name='Representante')
-    fecha_contrato = models.DateTimeField(default = date.today)
-    fecha_caducidad = models.DateTimeField(default=date.today)
+    fecha_contrato = models.DateField(default = timezone.now)
+    fecha_caducidad = models.DateField(null = True, blank= True, default= fecha_caducidad_cliente)
     activo = models.BooleanField(default=False)
     
     class Meta:
